9ca02d6fde00a9ace1e93b29e97e77b0620d6ddf
Xiaolu Dai <xiada@microsoft.com>
2024-08-07 21:26:22 +0800

    Add more env, fix eureka version

--- azure-runtime/extensions/spring/src/main/java/com/azure/runtime/host/extensions/spring/resources/SpringProject.java
+++ azure-runtime/extensions/spring/src/main/java/com/azure/runtime/host/extensions/spring/resources/SpringProject.java
@@ -31,7 +31,9 @@ public class SpringProject extends MicroserviceProject<SpringProject>
         
         super.getIntrospectOutputEnvs().forEach((k, v) -> {
             if ("BUILD_EUREKA_CLIENT_ENABLED".equals(k)) {
-                withEnvironment("EUREKA_CLIENT_SERVICEURL_DEFAULTZONE", "${services__eureka__http__0}/eureka/");
+                withEnvironment("eureka.client.serviceUrl.defaultZone", "{eureka.bindings.https.url}/eureka/");
+//                withEnvironment("EUREKA_INSTANCE_PREFERIPADDRESS", "true");
+                withEnvironment("CONFIG_SERVER_URL", "{config-server.bindings.http.url}");
             }
         });
         
